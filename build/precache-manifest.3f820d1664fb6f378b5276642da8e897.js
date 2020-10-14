d12cbbed823b74821de91bce32e5e57fde87bfec
dannyhoang <hnthai@humaxdigital.com>
2020-10-14 16:14:57 +0700

    update

---
+++ build/precache-manifest.3f820d1664fb6f378b5276642da8e897.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c3cc8f8ff08779ba2a80afb9ca1eaa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cef376b8a6df8d3b1909",
+    "url": "./static/css/2.e3cf4cf6.chunk.css"
+  },
+  {
+    "revision": "673a0def6e63fcf8913c",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cef376b8a6df8d3b1909",
+    "url": "./static/js/2.44445356.chunk.js"
+  },
+  {
+    "revision": "0aadc8a8405aeed76ae7e020b1d921e7",
+    "url": "./static/js/2.44445356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673a0def6e63fcf8913c",
+    "url": "./static/js/main.1c24614e.chunk.js"
+  },
+  {
+    "revision": "1cf86452c895420b5895",
+    "url": "./static/js/runtime-main.80619678.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
